0a5dad677ebb3bf496f02df8015ec7adcac3cdf2
Lidor Ashush <lidor12456@gmail.com>
2022-10-21 12:11:05 +0300

    gameboard works

--- app.js
+++ app.js
@@ -77,3 +77,89 @@
 //   drawObjects(8, 12, ["tree", "leaf"], tree);
 //   drawObjects(8, 17, ["tree", "leaf"], tree);
 //   drawObjects(9, 26, "wood", wood);
+
+
+
+
+
+
+
+
+
+
+
+
+const container = document.querySelector('.main-grid');
+
+const game = document.querySelector('.right-div');
+
+let inventory ={
+   rowTemp: 20,
+   colTemp: 15,
+   current: '',
+   tree: 'tree',
+   stone: 'stone',
+   ground: 'ground',
+   sky: '',
+   cloud: 'cloud', 
+   grass:'grass',
+   leaf:'leaf'
+}
+intializeGame()
+function intializeGame(){
+   for(let i = 0;i< inventory.rowTemp;i++){
+      for(let k = 0;k< inventory.colTemp;k++){
+         // container.style.display='grid';
+         // container.style.gridTemplate=`repeat(${inventory.rowTemp},1fr) / repeat(${inventory.colTemp},1fr)`
+         let div = document.createElement('div');
+         div.setAttribute('id',`cell${i}-${k}`)
+         div.setAttribute('class','cell')
+         // div.innerHTML = '';
+         container.appendChild(div)
+      }
+   } 
+}
+function fillCells(){
+   for(let k = 0;k< container.children.length;k++){
+      let [i,j] = container.children[k].getAttribute('id').slice(4).split('-');
+      
+      
+       fillGrass(i,j,k)
+       filltree(i,j,k)
+       fillleaf(i,j,k)
+   } 
+
+}
+function fillleaf(i,j,k){
+   if( i<8 && i>5 && j>7 && j<11 ){
+      container.children[k].classList.add(inventory.leaf);
+   }
+}
+
+function filltree(i,j,k){
+   if((i==8||i==11||i==10||i==9)&&j==9){
+      container.children[k].classList.add(inventory.tree);
+   }
+   
+}
+
+function fillGrass(i,j,k){
+   if(i==12){
+      container.children[k].classList.add(inventory.grass);
+   }
+   else if(i>=13){
+      container.children[k].classList.add(inventory.ground);
+   }
+}
+
+
+
+
+fillCells()
+
+
+container.addEventListener('click',(event)=>{
+   if(!event.target.getAttribute('class').includes('tree')){
+      event.target.setAttribute('class',inventory.tree);
+   }
+})
